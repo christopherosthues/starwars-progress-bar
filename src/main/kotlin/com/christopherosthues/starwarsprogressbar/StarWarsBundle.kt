28648000e3ce99f225b3d018f3f5b6ba1ab50d86
Christopher Osthues <christopher.osthues@gmail.com>
2023-04-15 19:08:37 +0200

    Add unit tests for message bundle

--- src/main/kotlin/com/christopherosthues/starwarsprogressbar/StarWarsBundle.kt
+++ src/main/kotlin/com/christopherosthues/starwarsprogressbar/StarWarsBundle.kt
@@ -8,8 +8,6 @@ import org.jetbrains.annotations.PropertyKey
 private const val BUNDLE = "messages.StarWarsBundle"
 
 internal object StarWarsBundle : AbstractBundle(BUNDLE) {
-    // TODO: unit test me
-    @Suppress("SpreadOperator")
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
         getMessage(key, *params)
---
+++ src/test/kotlin/com/christopherosthues/starwarsprogressbar/StarWarsBundleTests.kt
@@ -0,0 +1,90 @@
+package com.christopherosthues.starwarsprogressbar
+
+import com.intellij.BundleBase
+import com.intellij.idea.TestFor
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import io.mockk.verify
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+@TestFor(classes = [StarWarsBundle::class])
+internal class StarWarsBundleTests {
+
+    @BeforeEach
+    fun setup() {
+        mockkStatic(BundleBase::messageOrDefault)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkStatic(BundleBase::messageOrDefault)
+    }
+
+    @Test
+    fun `message with no parameters`() {
+        val key = "no_params_key"
+        val expectedMessage = "May the force be with you"
+        every { BundleBase.messageOrDefault(any(), key, any()) } returns expectedMessage
+
+        val result = StarWarsBundle.message(key)
+
+        assertEquals(expectedMessage, result)
+        verify(exactly = 1) { BundleBase.messageOrDefault(any(), key, any()) }
+    }
+
+    @Test
+    fun `message with parameters`() {
+        val key = "params_key"
+        val expectedMessage = "Hello, Luke Skywalker"
+        val name = "Luke"
+        val surname = "Skywalker"
+        every { BundleBase.messageOrDefault(any(), key, any(), name, surname) } returns expectedMessage
+
+        val result = StarWarsBundle.message(key, name, surname)
+
+        assertEquals(expectedMessage, result)
+        verify(exactly = 1) { BundleBase.messageOrDefault(any(), key, any(), name, surname) }
+    }
+
+    @Test
+    fun `message with empty key`() {
+        val key = ""
+        val expectedMessage = ""
+        every { BundleBase.messageOrDefault(any(), key, any()) } returns expectedMessage
+
+        val result = StarWarsBundle.message(key)
+
+        assertEquals(expectedMessage, result)
+        verify() { BundleBase.messageOrDefault(any(), key, any()) }
+    }
+
+    @Test
+    fun `message with invalid key`() {
+        val key = "invalid_key"
+        val expectedMessage = "!invalid_key!"
+        every { BundleBase.messageOrDefault(any(), key, any()) } returns expectedMessage
+
+        val result = StarWarsBundle.message(key)
+
+        assertEquals(expectedMessage, result)
+        verify(exactly = 1) { BundleBase.messageOrDefault(any(), key, any()) }
+    }
+
+    @Test
+    fun `message with non-string parameter`() {
+        val key = "params_key"
+        val expectedMessage = "Hello, 5 Skywalker"
+        val name = 5
+        val surname = "Skywalker"
+        every { BundleBase.messageOrDefault(any(), key, any(), name, surname) } returns expectedMessage
+
+        val result = StarWarsBundle.message(key, name, surname)
+
+        assertEquals(expectedMessage, result)
+        verify(exactly = 1) { BundleBase.messageOrDefault(any(), key, any(), name, surname) }
+    }
+}
