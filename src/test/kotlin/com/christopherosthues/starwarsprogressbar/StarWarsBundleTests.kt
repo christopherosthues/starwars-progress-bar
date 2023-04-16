1d89dfd878f45f982d422962f0a9854b273fac62
Christopher Osthues <christopher.osthues@gmail.com>
2023-04-16 14:12:47 +0200

    Cleanup tests

--- src/test/kotlin/com/christopherosthues/starwarsprogressbar/StarWarsBundleTests.kt
+++ src/test/kotlin/com/christopherosthues/starwarsprogressbar/StarWarsBundleTests.kt
@@ -26,64 +26,79 @@ internal class StarWarsBundleTests {
 
     @Test
     fun `message with no parameters`() {
+        // Arrange
         val key = "no_params_key"
         val expectedMessage = "May the force be with you"
         every { BundleBase.messageOrDefault(any(), key, any()) } returns expectedMessage
 
+        // Act
         val result = StarWarsBundle.message(key)
 
+        // Assert
         assertEquals(expectedMessage, result)
         verify(exactly = 1) { BundleBase.messageOrDefault(any(), key, any()) }
     }
 
     @Test
     fun `message with parameters`() {
+        // Arrange
         val key = "params_key"
         val expectedMessage = "Hello, Luke Skywalker"
         val name = "Luke"
         val surname = "Skywalker"
         every { BundleBase.messageOrDefault(any(), key, any(), name, surname) } returns expectedMessage
 
+        // Act
         val result = StarWarsBundle.message(key, name, surname)
 
+        // Assert
         assertEquals(expectedMessage, result)
         verify(exactly = 1) { BundleBase.messageOrDefault(any(), key, any(), name, surname) }
     }
 
     @Test
     fun `message with empty key`() {
+        // Arrange
         val key = ""
         val expectedMessage = ""
         every { BundleBase.messageOrDefault(any(), key, any()) } returns expectedMessage
 
+        // Act
         val result = StarWarsBundle.message(key)
 
+        // Assert
         assertEquals(expectedMessage, result)
         verify() { BundleBase.messageOrDefault(any(), key, any()) }
     }
 
     @Test
     fun `message with invalid key`() {
+        // Arrange
         val key = "invalid_key"
         val expectedMessage = "!invalid_key!"
         every { BundleBase.messageOrDefault(any(), key, any()) } returns expectedMessage
 
+        // Act
         val result = StarWarsBundle.message(key)
 
+        // Assert
         assertEquals(expectedMessage, result)
         verify(exactly = 1) { BundleBase.messageOrDefault(any(), key, any()) }
     }
 
     @Test
     fun `message with non-string parameter`() {
+        // Arrange
         val key = "params_key"
         val expectedMessage = "Hello, 5 Skywalker"
         val name = 5
         val surname = "Skywalker"
         every { BundleBase.messageOrDefault(any(), key, any(), name, surname) } returns expectedMessage
 
+        // Act
         val result = StarWarsBundle.message(key, name, surname)
 
+        // Assert
         assertEquals(expectedMessage, result)
         verify(exactly = 1) { BundleBase.messageOrDefault(any(), key, any(), name, surname) }
     }
