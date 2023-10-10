5c06d22fba06ed683f34a39d53b99bb15927b118
Christopher Osthues <christopher.osthues@gmail.com>
2023-10-10 20:26:38 +0200

    Add ktlint gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 import kotlin.collections.listOf
 
 fun properties(key: String) = providers.gradleProperty(key)
+
 fun environment(key: String) = providers.environmentVariable(key)
 
 plugins {
@@ -21,6 +23,8 @@ plugins {
     alias(libs.plugins.detekt)
     // Gradle Kover Plugin
     alias(libs.plugins.kover)
+    // ktlint Plugin
+    alias(libs.plugins.ktlint)
     // Build Plugins
     kotlin("plugin.serialization") version libs.versions.serialization.get()
 }
@@ -60,6 +64,18 @@ tasks.test {
     useJUnitPlatform()
     testLogging {
         events("passed", "skipped", "failed")
+    }
+}
+
+ktlint {
+    verbose = true
+    version = libs.versions.ktlint.get()
+    outputToConsole = true
+    coloredOutput = true
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
     }
 }
 
@@ -134,30 +150,32 @@ tasks {
         sinceBuild = properties("pluginSinceBuild")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+        pluginDescription =
+            providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
                 val start = "<!-- Plugin description -->"
                 val end = "<!-- Plugin description end -->"
 
-            with (it.lines()) {
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                with(it.lines()) {
+                    if (!containsAll(listOf(start, end))) {
+                        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                    }
+                    subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
                 }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
             }
-        }
 
         val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
-        changeNotes = properties("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
+        changeNotes =
+            properties("pluginVersion").map { pluginVersion ->
+                with(changelog) {
+                    renderItem(
+                        (getOrNull(pluginVersion) ?: getUnreleased())
+                            .withHeader(false)
+                            .withEmptySections(false),
                         Changelog.OutputType.HTML,
-                )
+                    )
+                }
             }
-        }
     }
 
     runPluginVerifier {
